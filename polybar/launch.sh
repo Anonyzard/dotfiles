792f79e9a57c8a065627f91c315ec2ea25637942
José Leiva <anonyzard@disroot.org>
2023-10-25 20:55:00 -0300

    polybar: reestructure config, adding new modules and turn off launch of hdmi bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,9 +8,9 @@ pkill polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar -c ~/.config/polybar/anz-config.ini anonyzard 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c ~/.config/polybar/anz-config.ini hdmi 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar -c ~/.config/polybar/anz-config.ini hdmi 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 #pgrep -x wpa-cute || wpa-cute -t &
-pgrep -x connman-gtk || connman-gtk &
+# pgrep -x connman-gtk || connman-gtk &
 #pgrep -x cmst || cmst -m &
