3191f8c856dbcc51c24bdd70c99032820b4c78a7
José Leiva <anonyzard@disroot.org>
2024-03-13 15:42:50 -0300

    polybar: Simplify modules and add uptime module
    Note: uptime module needs neofetch

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Terminate already running bar instances (killall -q polybar)
-pkill polybar
+pkill polybar 
 
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
+date > /tmp/polybar.log
 polybar -c ~/.config/polybar/anz-config.ini anonyzard 2>&1 | tee -a /tmp/polybar.log & disown
-# polybar -c ~/.config/polybar/anz-config.ini hdmi 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
-#pgrep -x wpa-cute || wpa-cute -t &
-# pgrep -x connman-gtk || connman-gtk &
-#pgrep -x cmst || cmst -m &
